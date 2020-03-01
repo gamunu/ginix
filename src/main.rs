413caff7609128f1bf4bd73b0a3b3bfb2d789169
Gamunu Balagalla <4501687+gamunu@users.noreply.github.com>
2020-03-01 13:18:02 +0530

    Add VGA buffer to the kernel

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disabled all Rust-level entry points
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World";
+use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
 /// This function called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
