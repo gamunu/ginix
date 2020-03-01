1a0f782fddad3b8084c7574b3b0e798bd90ddb12
Gamunu Balagalla <4501687+gamunu@users.noreply.github.com>
2020-03-01 16:21:35 +0530

    Implement testing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disabled all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(ginix::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
+use ginix::println;
 use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// This function called on xtest panic
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ginix::test_panic_handler(info)
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,9 +1,18 @@
 use core::fmt;
-
-// in src/vga_buffer.rs
 use volatile::Volatile;
 use lazy_static::lazy_static;
 use spin::Mutex;
+
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::White, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -110,7 +119,7 @@ impl Writer {
             color_code: self.color_code,
         };
         for col in 0..BUFFER_WIDTH {
-            self.buffer.chars[row][col].write(blank)
+            self.buffer.chars[row][col].write(blank);
         }
     }
 }
@@ -122,13 +131,6 @@ impl fmt::Write for Writer {
     }
 }
 
-lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::White, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    });
-}
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
@@ -140,8 +142,38 @@ macro_rules! println {
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
-
+#[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap()
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_simple() {
+    serial_println!("test_println...");
+    println!("test_println_simple output");
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_many() {
+    serial_print!("test_println_many...");
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output() {
+    serial_print!("test_println_output...");
+
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i,c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]");
 }
